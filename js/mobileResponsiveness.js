19d5e516f1c14a00741e44e61bc876265d894100
DomizianoScarcelli <44399141+DomizianoScarcelli@users.noreply.github.com>
2021-04-12 17:10:55 +0200

    Renaming confusing css classes and ids

--- js/mobileResponsiveness.js
+++ js/mobileResponsiveness.js
@@ -1,5 +1,5 @@
 /*Rimuove la classe row-cols-3 dal top-restaurants-nearby se la larghezza della finestra è minore di 580 pixel in quanto creava un bug */
 $(window).on('resize', function () {
     var win = $(this); //this = window
-    if (win.width() < 580) { $('#top-restaurants-nearby').removeClass('row-cols-3'); }
+    if (win.width() < 580) { $('#nearby-suggestions-card-list').removeClass('row-cols-3'); }
 });
