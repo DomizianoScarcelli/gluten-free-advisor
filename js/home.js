2719bbe3e1edeb14b98631a856f414a9abc073fb
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-04-14 20:16:18 +0200

    Minor changes

---
+++ js/home.js
@@ -0,0 +1,5 @@
+const addRestaurantButton = document.getElementById("add-restaurant-button");
+
+addRestaurantButton.addEventListener("click", () => {
+    location.pathname = "/add-restaurant.html";
+})
