a692cf82811284341e04dd0be4f8427b04c27cec
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-04-19 11:23:39 +0200

    Minor changes

--- index.php
+++ index.php
@@ -20,14 +20,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
     <!--Fonts-->
     <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet">
@@ -51,11 +51,11 @@
     <!--Ristoranti consigliati-->
     <div id="suggestions-main-container">
         <!--I nostri consigli-->
-        <?php include "assets/home/restaurant-suggestions.html";?>
+        <?php include "assets/home/restaurant-suggestions.html"; ?>
 
         <!--I migliori ristoranti vicino a te-->
-        <?php include "assets/home/nearby-restaurants.html";?>
-        
+        <?php include "assets/home/nearby-restaurants.html"; ?>
+
     </div>
     <!--Pulsante per il form per aggiungere ristoranti-->
     <div id="add-restaurant-button-container">
@@ -67,7 +67,7 @@
     </div>
     <!--Codice PHP che inserisce il form modale modal-form.php-->
     <?php include 'assets/home/modal-form.html'; ?>
-    
+
 </body>
 
 </html>
