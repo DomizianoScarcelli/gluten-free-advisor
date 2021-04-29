cb3c0df04636f4da098cd08f36ff34a7ba9d0217
Domiziano Scarcelli <scarcelli.d@outlook.com>
2021-04-29 23:32:17 +0200

    Fixed minor bug

--- js/search.js
+++ js/search.js
@@ -6,9 +6,11 @@ const searchbar = document.getElementById("searchbar");
 
 var urlParams = new URLSearchParams(location.search)
 
+
 searchbar.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
         search();
+
     }
 })
 
@@ -16,13 +18,12 @@ searchbar.addEventListener("keypress", (event) => {
  * Setta la querystring se in index, prende le info dalla querystring ed effettua una ricerca nel database mostrando i risultati nella search-page
  */
 function search() {
-    if (location.pathname == '/index.php') { //Se siamo nell'index allora setta la querystring
+    if (location.pathname == '/index.php' || location.pathname == '/') { //Se siamo nell'index allora setta la querystring
         //Genera querystring
         urlParams.append('query', searchbar.value);
         //Reindirizza in search-page.php/querystring
         let url = new URL(location.origin + '/search-page.php?' + urlParams);
         location.href = url;
-
     } else { //Altrimenti prendi i valori dalla querystring
         urlParams.delete('query');
         urlParams.append('query', searchbar.value);
@@ -33,27 +34,26 @@ function search() {
 /**
  * Animazione che fa comparire e sparire il bottone di ricerca a seconda se ci sia o meno testo nella barra di ricerca
  */
-$('#searchbar').on('input', function (e) {
+$('#searchbar').on('input', function(e) {
+
     if (searchbar.value == '' && document.getElementById("searchButton")) {
+
         $('#searchButton').hide(300);
         setTimeout(() => {
             $(document.getElementById("searchButton")).remove();
         }, 300)
         $('#search-container').css('height', '15rem');
 
-    }
-    else if (searchbar.value != '' && !document.getElementById("searchButton")) {
-        if (location.pathname == '/index.php') {
+    } else if (searchbar.value != '' && !document.getElementById("searchButton")) {
+        console.log('piero')
+        if (location.pathname == '/index.php' || location.pathname == '/') {
             let button = `<button class="btn search-btn" style="display:none;" id="searchButton" onclick='search()'>Ricerca</button>`;
             $(button).appendTo(document.getElementById("search-container")).show(300);
             $('#search-container').css('height', '18rem');
-        }
-        else {
+        } else {
             let button = `<button class="btn search-btn invert-btn" style="display:none;" id="searchButton" onclick='search()'>Ricerca</button>`;
             $(button).appendTo(document.getElementById("header")).show(300);
         }
 
     }
 });
-
-
