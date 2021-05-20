f932eb56d9ef37f8ba4d8909bf3fc9ef345a8737
CristianaDiTullio <cristiana.ditullioft@gmail.com>
2021-05-20 11:56:22 +0200

    Now review form really adds reviews to db

--- ristoranti/addReview.js
+++ ristoranti/addReview.js
@@ -7,31 +7,22 @@ const closeButton = document.getElementById('close-button');
 const modalBody = document.getElementById('modal-body');
 const originalHtml = modalBody.innerHTML;
 
-/*
-const restaurantName = document.getElementById('restaurant-name');
-const restaurantCity = document.getElementById('restaurant-address');
-*/
-
 submitReviewButton.addEventListener('click', function () {
     //Se il pulsante è già stato premuto, allora chiudi il form modale e resetta i campi.
     if (submitReviewButton.value == 'Chiudi') {
         location.reload();
     }
     else {
-        /*Altrimenti invia i dati tramite il submit.
+        /*Altrimenti invia i dati tramite il submit. */
         if ($('#review-title').val() == '' || $('#review-date').val() == '' || $('#review-text').val() == '') {
             alert('Titolo, data e descrizione sono obbligatori!');
             return;
         }
-        else {
-            return;
-        }*/
+        console.log('recensione aggiunta');
+        submit();
+        submitReviewButton.removeAttribute('data-dismiss');
+        submitReviewButton.removeAttribute('Aria-label');
     }
-    console.log('recensione aggiunta');
-    submit();
-    /*
-    submitReviewButton.removeAttribute('data-dismiss');
-    submitReviewButton.removeAttribute('Aria-label'); */
 });
 
 closeButton.addEventListener('click', () => {
@@ -42,7 +33,7 @@ function submit() {
     console.log('Added');
     sendReviewData();
     modalBody.innerHTML = `
-        <p style="text-align:center;" >Grazie per il tuo contributo, la recensione è stata salvata e aggiunta correttamente!</p>
+        <p style="text-align:center;" >Grazie per il tuo contributo, la recensione è stata aggiunta al profilo del ristorante!</p>
     `;
     submitReviewButton.value = 'Chiudi';
     submitReviewButton.innerHTML = 'Chiudi';
@@ -52,19 +43,19 @@ function sendReviewData() {
     var formData = new FormData();
 
     //Prendo il valore dei campi di input del form
-    var id_ristorante = document.getElementsByName('restaurant-div').id;
-    var title = $('#review-title').value;
-    var text = $('#review-text').value;
+    var id_ristorante = document.getElementsByName('restaurant-div')[0].id;
+    var title = $('#review-title').val();
+    var text = $('#review-text').val();
     var radiovalues = document.getElementsByName('inlineRadioOptions');
     for (var i = 0; i < radiovalues.length; i++) {
         if (radiovalues[i].checked) {
             var rating = radiovalues[i].value;
         }
     }
-    var date = $('#review-date').value;
-    var username = $('#username').value;
+    var date = $('#review-date').val();
+    var username = $('#username').val();
 
-    //Li inserisco dentro formData
+    //Li inserisco dentro l'oggetto formData
     formData.append('id_ristorante', id_ristorante)
     formData.append('title', title);
     formData.append('text', text);
@@ -72,7 +63,7 @@ function sendReviewData() {
     formData.append('date', date);
     formData.append('username', username);
 
-    //Ajax
+    //Faccio la post con il metodo $.ajax() di JQuery
     $.ajax({
         type: 'POST',
         url: 'dbAddReview.php',
