bdaf1ba2d51a0b588855e99e4f62e597a2746efd
CristianaDiTullio <cristiana.ditullioft@gmail.com>
2021-05-24 12:15:55 +0200

    Fixed apostrophe bug in dbSendMail

--- dbManager/dbSendMail.php
+++ dbManager/dbSendMail.php
@@ -1,10 +1,10 @@
 <?php
 include 'dbConnect.php';
 
-$name = htmlspecialchars ($_POST['name']);
-$subject = htmlspecialchars ($_POST['subject']);
-$email = htmlspecialchars ($_POST['email']);
-$message = htmlspecialchars ($_POST['message']);
+$name = str_replace("'", "''", $_POST['name']);
+$subject = str_replace("'", "''", $_POST['subject']);
+$email = str_replace("'", "''", $_POST['email']);
+$message = str_replace("'", "''", $_POST['message']);
 
 
 $sql = "INSERT INTO `messaggi` (`nome`, `email`, `subject`, `message`) VALUES ('$name', '$email', '$subject', '$message')";
